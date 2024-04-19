7b8b8d185d2ae9edfc0a61730c52ecdd2c79f18b
Henri <u19217724@gmail.com>
2024-04-19 02:17:55 -0500

    avance del proyecto

--- components/Map/MapBoxRoute.tsx
+++ components/Map/MapBoxRoute.tsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import Map, { AttributionControl, GeolocateControl , Layer, Marker, NavigationControl,
-    Popup, Source } from "react-map-gl";
-function MapBoxRoute(props:any) {
-
-
+import {
+  Layer,
+  Source
+} from "react-map-gl";
+function MapBoxRoute(props: any) {
   return (
-    <Source type="geojson" data={{ type: 'Feature', geometry: 
-{ type: 'LineString', coordinates: props.coordinates } }}>
-        <Layer
-          type="line"
-          layout={{ 'line-join': 'round', 'line-cap': 'square' }}
-          paint={{ 'line-color': '#0462d4', 'line-width': 4 }}
-        />
-      </Source>
-  )
+    <Source
+      type="geojson"
+      data={{
+        type: "Feature",
+        geometry: { type: "LineString", coordinates: props.coordinates },
+        properties: {} // Agrega una propiedad "properties"
+      }}
+    >
+      <Layer
+        type="line"
+        layout={{ "line-join": "round", "line-cap": "square" }}
+        paint={{ "line-color": "#0462d4", "line-width": 4 }}
+      />
+    </Source>
+  );
 }
 
-export default MapBoxRoute
+export default MapBoxRoute;
